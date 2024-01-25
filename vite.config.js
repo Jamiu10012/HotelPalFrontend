3b1f02dd56d2554ef017126f9fdfd696e8cae93e
jamiu10012 <ibrahimjamiu026@gmail.com>
2024-01-25 12:07:15 +0100

    on some stuff

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    minify: "terser", // Enable minification using Terser
+    sourcemap: true, // Generate source maps for better debugging in production
+    terserOptions: {
+      compress: {
+        drop_console: true, // Remove console.log statements in production
+      },
+    },
+  },
+
+  css: {
+    modules: false, // Set to true if you want to enable CSS modules
+    extract: true, // Extract CSS into a separate file in production
+  },
+});
